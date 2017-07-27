baa813862eedebbaa9d38ecf9dd68c74e1831c27
Angela Vogler <angelavogler4@gmail.com>
2017-07-27 13:42:18 -0400

    finished daily project

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let numbers = [ 1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6 ];
 */
 function getFourthNum(){
     // Your answer here:
+    return numbers[3];
 }
 
 /*
@@ -24,6 +25,12 @@ function getFourthNum(){
 function smallNums(){
     let smallNums = [];
     // Your answer here:
+    for (let i = 0; i < numbers.length; i++){
+      if (numbers[i] < 10) {
+        smallNums.push(numbers[i]);
+      }
+    }
+    return smallNums;
 }
 
 /*
@@ -31,6 +38,8 @@ function smallNums(){
 */
 function addNums(){
     // Your answer here:
+    numbers.push(12, 99, 101);
+    return numbers;
 }
 
 
@@ -54,6 +63,8 @@ let film = {
 */
 function addBoxOffice(){
     // Your answer here:
+    film.boxoffice = 269061;
+    return film;
 }
 
 /*
@@ -61,13 +72,17 @@ function addBoxOffice(){
 */
 function addActor(){
     // Your answer here:
+    film.actors.push("Yukiko Shimazaki");
+    return film;
 }
 
 /*
-6. Now that you've added a "boxoffice" property, subtract "budget" from "property" and return the difference. This number is the amount lost by the studio in making the film. Return the loss.
+6. Now that you've added a "boxoffice" property, subtract "budget" from "boxoffice" and return the difference.
+This number is the amount lost by the studio in making the film. Return the loss.
 */
 function getLosses(){
     // Your answer here:
+    return film.boxoffice - film.budget;
 }
 
 
@@ -87,15 +102,16 @@ let letterVals = [ "v", "x", "r", "f", "p" ];
 function interleave(){
     let vals = [];
     // Your answer here:
+    for (let i = 0; i < numberVals.length; i++){
+      vals[i] = (letterVals[i] + numberVals[i]);
+    }
+    return vals;
 }
 
 
-
-
-
-
 /*
-8. Iterate over the "first" and "second" arrays. Compare the values for both arrays. If the arrays values are the same, then store that value in the "same" array. Return "same".
+8. Iterate over the "first" and "second" arrays. Compare the values for both arrays.
+If the arrays values are the same, then store that value in the "same" array. Return "same".
 */
 
 let first = [ "blink", "stand", "glasses", "chair", "numinous", "adjacent", "bracelet", "hand" ];
@@ -104,4 +120,10 @@ let second = [ "think", "stand", "cheese", "break", "numinous", "mouse", "close"
 function union(){
     let same = [];
     // Your answer here:
+    for (let i = 0; i < first.length; i++){
+      if (first[i] === second[i]) {
+        same.push(first[i]);
+      }
+    }
+    return same;
 }
